132d202210a3c86d69246270fad61f0c700c3119
Danilo Silva <DaniloCostaS>
2026-03-27 00:05:46 -0300

    Comentando cartas globais

--- src/constants/cards.ts
+++ src/constants/cards.ts
@@ -53,6 +53,6 @@ export const CARDS_DB: CardData[] = [
     { id: 'new_leader', name: "Novo líder", icon: "⚔️", desc: "Escolha um jogador: Se vencer a batalha, pegue uma insígnia aleatória que ainda não tem do adversário.", type: 'move' },
 
     // Cartas Globais
-    { id: 'communism', name: "Comunismo", icon: "♻️", desc: "Junta todas as cartas de todos os jogadores, embaralha e redistribui igualmente. As que sobrarem são destruídas.", type: 'global' },
-    { id: 'imposto', name: "Imposto de Renda", icon: "📜", desc: "A Receita Federal te achou! TODOS os jogadores perdem exatamente a metade (espalhada aleatoriamente) de suas cartas e metade de todos os seus itens.", type: 'global' }
+    //{ id: 'communism', name: "Comunismo", icon: "♻️", desc: "Junta todas as cartas de todos os jogadores, embaralha e redistribui igualmente. As que sobrarem são destruídas.", type: 'global' },
+    //{ id: 'imposto', name: "Imposto de Renda", icon: "📜", desc: "A Receita Federal te achou! TODOS os jogadores perdem exatamente a metade (espalhada aleatoriamente) de suas cartas e metade de todos os seus itens.", type: 'global' }
 ];
